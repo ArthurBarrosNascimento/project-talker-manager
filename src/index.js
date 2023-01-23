a776d43a0cba5be4efa4567e11bd323a8928ec71
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2023-01-23 16:07:48 -0300

    req dois feito

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const ERRO = 'Pessoa palestrante não encontrada';
+
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -17,8 +19,17 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
-app.get('/talker', (req, res) => {// exibe todo o array
+app.get('/talker', (_req, res) => {// exibe todo o array
   if (talke.length === 0) res.status(200).json([]);
 
   return res.status(200).json(talke);
+});
+
+app.get('/talker/:id', (req, res) => {// filtra array pelo Id
+  const { id } = req.params;
+
+  const findTalkerById = talke.find((t) => t.id === Number(id));
+  if (!findTalkerById) res.status(404).json({message: ERRO});
+
+  return res.status(200).json(findTalkerById);
 });
