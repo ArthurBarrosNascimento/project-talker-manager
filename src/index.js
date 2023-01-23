c8a68c5ad54342e8e76c377f3dd1fbe1e06f7371
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2023-01-23 16:02:11 -0300

    primeira validação do req um feito

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const talke = require('./talker.json');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -13,4 +15,10 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', (req, res) => {// exibe todo o array
+  if (talke.length === 0) res.status(200).json([]);
+
+  return res.status(200).json(talke);
 });
