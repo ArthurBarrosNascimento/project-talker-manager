9140866189cb57de0b1e95becf330effab656e86
Arthur de Barros <arthurbarros.nascimento12@gmail.com>
2023-01-23 16:42:06 -0300

    req três feito

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const talke = require('./talker.json');
+const geraToken = require('./service');
 
 const app = express();
 app.use(express.json());
@@ -32,4 +33,14 @@ app.get('/talker/:id', (req, res) => {// filtra array pelo Id
   if (!findTalkerById) res.status(404).json({message: ERRO});
 
   return res.status(200).json(findTalkerById);
+});
+
+app.post('/login', (req, res) => {
+  const result = req.body;
+  
+  if (Object.keys(result).length === 0) return res.status(401).end();
+
+  const toke = geraToken(16);
+  return res.status(200).json({ token: toke});
+  
 });
---
+++ src/service.js
@@ -0,0 +1,12 @@
+function geraStringAleatoria(tamanho) {
+  let stringAleatoria = '';
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < tamanho; i += 1) {
+      stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return stringAleatoria;
+}
+
+console.log(geraStringAleatoria(16));
+
+module.exports = geraStringAleatoria;
